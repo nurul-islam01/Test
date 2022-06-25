52b93652d174e0f028290e2e66687c8cf3d137ea
nurul-islam-prothomalo <nurul.islam3f@gmail.com>
2022-06-25 22:29:42 +0600

    added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.webp";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default App;
